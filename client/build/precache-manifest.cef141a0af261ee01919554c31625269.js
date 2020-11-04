5760244103e9ddfba775ba6007f37b51c5850fb0
fayeazahmed <r10.ahmed@yahoo.com>
2020-11-05 00:38:47 +0600

    make it better

---
+++ client/build/precache-manifest.cef141a0af261ee01919554c31625269.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10784b35901a1eab248a9048df513679",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bc68691f5aac737aa6c8",
+    "url": "/static/css/main.5480a015.chunk.css"
+  },
+  {
+    "revision": "17d0ecc2dbdae0c5c2d3",
+    "url": "/static/js/2.285a434f.chunk.js"
+  },
+  {
+    "revision": "0fb0e672f1308cb5b7677cdb93fc42ab",
+    "url": "/static/js/2.285a434f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc68691f5aac737aa6c8",
+    "url": "/static/js/main.a677d7c3.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
