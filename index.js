6766363fd435253bc0f982563f5adcbf32832b28
fayeazahmed <r10.ahmed@yahoo.com>
2020-11-05 00:08:23 +0600

    last one i guess

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ app.use(middlewares.errorHandler);
 const port = process.env.PORT || 1337;
 
 if(process.env.NODE_ENV == "production") {
-	app.use(express.static("/client/build"))
+	app.use(express.static("client/build"))
 	app.get("*", (req, res) => {
 		res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
 	})
