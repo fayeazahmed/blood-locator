4e717955ba11438c34a761070285abbe8a959091
fayeazahmed <r10.ahmed@yahoo.com>
2020-11-04 23:09:49 +0600

    changed directory

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ const port = process.env.PORT || 1337;
 if(process.env.NODE_ENV == "production") {
 	app.use(express.static("/client/build"))
 	app.get("*", (req, res) => {
-		res.sendFile(path.resolve(__dirname, "../client", "build", "index.html"))
+		res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
 	})
 }
 
